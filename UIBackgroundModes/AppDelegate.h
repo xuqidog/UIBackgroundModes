8d054ea69c857c7004bc77a495d69ab14d3b5fc5
365-lmj <365-Xuqidong@365-lmjdemacbook-pro.local>
2016-06-08 16:02:10 +0800

    Initial Commit

---
+++ UIBackgroundModes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIBackgroundModes
+//
+//  Created by 徐其东 on 16/6/8.
+//  Copyright © 2016年 徐其东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIBackgroundModes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIBackgroundModes
+//
+//  Created by 徐其东 on 16/6/8.
+//  Copyright © 2016年 徐其东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
